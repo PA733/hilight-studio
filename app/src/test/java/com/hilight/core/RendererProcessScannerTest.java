6b4d72cf56d33aa957bbf00360c41640c4fcf396
PA733 <67588574+PA733@users.noreply.github.com>
2026-09-07 16:15:52 +0800

    test: run the process-scanner regression harness in JUnit

---
+++ app/src/test/java/com/hilight/core/RendererProcessScannerTest.java
@@ -0,0 +1,9 @@
+package com.hilight.core;
+
+import org.junit.Test;
+
+public final class RendererProcessScannerTest {
+    @Test public void processDiscoveryRegressionSuite() throws Exception {
+        RendererProcessScannerSelfTest.runAll();
+    }
+}
